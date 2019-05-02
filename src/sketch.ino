41f8166a1eb6efe2609d76ee96c74adfdd4e2600
Arturo Vargas Cuevas <a01652564@tec.mx>
2019-05-02 16:00:00 -0500

    test: Add servo motor control test

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,86 +1,28 @@
 /**
- * Title: Phase 1 - PIR Motion Sensor Test
+ * Title: Phase 2 - Servo Motor Control Test
  *
- * Brief: Tests and logs motion detection using a passive infrared (PIR) sensor. 
- *        Includes 30-second calibration, real-time motion event logging via serial,
- *        and LED visualization. Detects motion end after 5 seconds of inactivity.
+ * Brief: Tests servo motor control on pin 8. Rotates between 0 and 180 degrees 
+ *        with 1-second intervals. Includes initial 2-second calibration delay.
  *
  * Author: Arturo Vargas Cuevas (a01652564@tec.mx)
  * Date:   2019-05-02
  */
 
+ #include <Servo.h>
 
- //the time we give the sensor to calibrate (10-60 secs according to the datasheet)
-int calibrationTime = 30;        
- 
-//the time when the sensor outputs a low impulse
-long unsigned int lowIn;         
- 
-//the amount of milliseconds the sensor has to be low 
-//before we assume all motion has stopped
-long unsigned int pause = 5000;  
- 
-boolean lockLow = true;
-boolean takeLowTime;  
- 
-int pirPin = 3;    //the digital pin connected to the PIR sensor's output
-int ledPin = 13;
- 
- 
-/////////////////////////////
-//SETUP
-void setup(){
-  Serial.begin(9600);
-  pinMode(pirPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(pirPin, LOW);
- 
-  //give the sensor some time to calibrate
-  Serial.print("calibrating sensor ");
-    for(int i = 0; i < calibrationTime; i++){
-      Serial.print(".");
-      delay(1000);
-      }
-    Serial.println(" done");
-    Serial.println("SENSOR ACTIVE");
-    delay(50);
-  }
- 
-////////////////////////////
-//LOOP
-void loop(){
- 
-     if(digitalRead(pirPin) == HIGH){
-       digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
-       if(lockLow){  
-         //makes sure we wait for a transition to LOW before any further output is made:
-         lockLow = false;            
-         Serial.println("---");
-         Serial.print("motion detected at ");
-         Serial.print(millis()/1000);
-         Serial.println(" sec"); 
-         delay(50);
-         }         
-         takeLowTime = true;
-       }
- 
-     if(digitalRead(pirPin) == LOW){       
-       digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
- 
-       if(takeLowTime){
-        lowIn = millis();          //save the time of the transition from high to LOW
-        takeLowTime = false;       //make sure this is only done at the start of a LOW phase
-        }
-       //if the sensor is low for more than the given pause, 
-       //we assume that no more motion is going to happen
-       if(!lockLow && millis() - lowIn > pause){  
-           //makes sure this block of code is only executed again after 
-           //a new motion sequence has been detected
-           lockLow = true;                        
-           Serial.print("motion ended at ");      //output
-           Serial.print((millis() - pause)/1000);
-           Serial.println(" sec");
-           delay(50);
-           }
-       }
-  }
+Servo servo;
+void setup() {
+  servo.attach(8);
+  servo.write(0);
+  delay(2000);
+
+}
+
+void loop() {
+
+  servo.write(180);
+  delay(1000);
+  servo.write(0);
+  delay(1000); 
+
+}
